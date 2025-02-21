c434eadb429b31dc64eef078e54fd42346c5f7cc
CHEPIK27 <nikitachepikov11@gmail.com>
2025-02-21 18:10:19 +0200

    Add files via upload

---
+++ src/LAB_5/LAB_5/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+class Program
+{
+    // Завдання 1: Перевірка парності числа
+    static bool IsEven(int number)
+    {
+        return number % 2 == 0;
+    }
+
+    // Завдання 2: Перевантаження функції (Overloading)
+    static int Sum(int a, int b)
+    {
+        return a + b;
+    }
+
+    static int Sum(int a, int b, int c)
+    {
+        return a + b + c;
+    }
+
+    static double Sum(double a, double b)
+    {
+        return a + b;
+    }
+
+    // Завдання 3: Функція з ref
+    static void Swap(ref int a, ref int b)
+    {
+        int temp = a;
+        a = b;
+        b = temp;
+    }
+
+    static void Main()
+    {
+        // Перевірка IsEven
+        Console.WriteLine(IsEven(8)); 
+        Console.WriteLine(IsEven(7)); 
+
+        // Перевірка Sum
+        Console.WriteLine(Sum(5, 10)); 
+        Console.WriteLine(Sum(2, 3, 4)); 
+        Console.WriteLine(Sum(2.5, 3.1)); 
+
+        // Перевірка Swap
+        int a = 5, b = 10;
+        Swap(ref a, ref b);
+        Console.WriteLine($"a = {a}, b = {b}"); // Очікуваний результат: a = 10, b = 5
+    }
+}

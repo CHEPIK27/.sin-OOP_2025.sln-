945f81cf172ef74899264e1f785da9e2743ac124
CHEPIK27 <nikitachepikov11@gmail.com>
2025-04-02 23:37:11 +0300

    Add files via upload

---
+++ src/LAB_13.1/LAB_13.1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+class Order
+{
+    public event EventHandler<string> OrderStatusChanged;
+    private string status;
+
+    public string Status
+    {
+        get => status;
+        set
+        {
+            if (status != value)
+            {
+                status = value;
+                OnOrderStatusChanged(status);
+            }
+        }
+    }
+
+    protected virtual void OnOrderStatusChanged(string status)
+    {
+        OrderStatusChanged?.Invoke(this, status);
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Order order = new Order();
+        order.OrderStatusChanged += OrderStatusChangedHandler;
+
+        order.Status = "Замовлення отримано";
+        order.Status = "Відправлено";
+        order.Status = "Доставлено";
+    }
+
+    static void OrderStatusChangedHandler(object sender, string status)
+    {
+        Console.WriteLine($"Статус замовлення змінено на: {status}");
+    }
+}
---
+++ src/LAB_13/LAB_13/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+//завдання №1
+
+
+class Program
+{
+    static void Main()
+    {
+        int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+        Predicate<int> isEven = n => n % 2 == 0;
+        var evenNumbers = Filter(numbers, isEven);
+
+        Console.WriteLine("Парні числа:");
+        foreach (var num in evenNumbers)
+        {
+            Console.WriteLine(num);
+        }
+    }
+
+    static int[] Filter(int[] numbers, Predicate<int> predicate)
+    {
+        var result = new List<int>();
+        foreach (var number in numbers)
+        {
+            if (predicate(number))
+                result.Add(number);
+        }
+        return result.ToArray();
+    }
+}
+
+

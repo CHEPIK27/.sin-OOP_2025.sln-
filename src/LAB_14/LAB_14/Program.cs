11368885f992c09fe3d968df63474fa585dc84eb
CHEPIK27 <nikitachepikov11@gmail.com>
2025-04-03 00:31:23 +0300

    Add files via upload

---
+++ src/LAB_14/LAB_14/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+class Program
+{
+    static async Task Main()
+    {
+        string[] files = { "log1.txt", "log2.txt", "log3.txt" };
+        var tasks = files.Select(f => Task.Run(() => ProcessFile(f))).ToArray();
+        await Task.WhenAll(tasks);
+        Console.WriteLine("Обробка завершена!");
+    }
+
+    static void ProcessFile(string file)
+    {
+        int errors = File.ReadAllLines(file).Count(line => line.Contains("ERROR"));
+        Console.WriteLine($"{file}: знайдено {errors} помилок.");
+    }
+}
